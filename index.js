f41972dabe804b563420066a51ef034541630fe9
Kyle Coberly <kyle.coberly@gmail.com>
2020-07-06 10:26:23 -0600

    Getting rid of solution

--- index.js
+++ index.js
@@ -5,26 +5,10 @@
 //  Uses pure functions
 //  Uses no anonymous inline functions
 
-// var teachers = ["Kyle", "Potts", "Damon", "Dina", "Vui", "Mark"];
+var teachers = ["Kyle", "Potts", "Damon", "Dina", "Vui", "Mark"];
 
-// var loudTeacher = ""
-// for (var i = 0; i < teachers.length; i++) {
-//     loudTeacher = teachers[i] + "!"
-//     console.log(loudTeacher);
-// }
-
-getTeachers()
-    .map(loudify)
-    .forEach(announce)
-
-function getTeachers(){
-    return ["Kyle", "Potts", "Damon", "Dina", "Vui", "Mark"]
-}
-
-function loudify(string){
-    return `${string}!`
-}
-
-function announce(string){
-    console.log(string)
+var loudTeacher = ""
+for (var i = 0; i < teachers.length; i++) {
+    loudTeacher = teachers[i] + "!"
+    console.log(loudTeacher);
 }
